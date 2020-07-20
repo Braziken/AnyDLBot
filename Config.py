e6b63b989c84bba5fdf108eca6b0c6a1e7b12202
Braziken <51787563+Braziken@users.noreply.github.com>
2020-07-20 13:37:27 -0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,9 +1,9 @@
 from anydlbot.sample_config import Config
 
 class Development(Config):
-  APP_ID = 6
-  API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
-  TG_BOT_TOKEN = ""
+  APP_ID = 944105
+  API_HASH = 2f58fe1375b7cddd2c55fb3e51c647e9"944105
+  TG_BOT_TOKEN = "448349507:AAHHXvUY2es0ePlgevSS-uqPuvU74zvnGI8"
   AUTH_USERS = [
-    7351948
+    244737355
   ]
